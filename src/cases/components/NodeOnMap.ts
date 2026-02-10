9627e933c8a090fd5c481cfcb31f1393d22e8cfa
Eugene Kosukhin <kosuha606@gmail.com>
2026-02-10 21:52:28 +0300

    fix: node position

--- src/cases/components/NodeOnMap.ts
+++ src/cases/components/NodeOnMap.ts
@@ -4,7 +4,9 @@ import { html } from "silentium-ui";
 import { TheNodeWithTemplate } from "../../domain/Node";
 
 export function NodeOnMap(node$: MessageType<TheNodeWithTemplate>) {
-    return Template((t) => html`<div style="">
+    const top$ = Path(node$, 'node.position.0');
+    const left$ = Path(node$, 'node.position.1');
+    return Template((t) => html`<div class="absolute" style="left: ${t.escaped(left$)}px;top: ${t.escaped(top$)}px">
         ${t.raw(Path(node$, 'template'))}
     </div>`)
 }
