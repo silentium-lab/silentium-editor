4626ae095bd580e71f3d9b3890f3449e91e76c41
Eugene Kosukhin <kosuha606@gmail.com>
2026-02-15 16:35:09 +0300

    fix: mini map is not selectable

--- src/cases/pages/EditPage.ts
+++ src/cases/pages/EditPage.ts
@@ -22,7 +22,7 @@ export function EditPage(content$: MessageSourceType<string>) {
     return Connected(Template((t) => `<div class="bg-base-inverse grid grid-rows-[50px_1fr] grid-cols-[200px_1fr] h-screen">
         <div class="col-span-2 bg-secondary">${t.raw(NavigationPanel())}</div>
         <div class="w-40 relative z-10 bg-secondary">${t.raw(TypesPanel(types$))}</div>
-        <div class="absolute pointer-events-none bottom-2 right-2 w-26 h-26 border z-50 bg-base">${t.raw(MiniMap())}</div>
+        <div class="absolute pointer-events-none bottom-2 right-2 w-26 h-26 border z-50 bg-base select-none">${t.raw(MiniMap())}</div>
         <div class="${t.escaped(canvasId$)} nodes-view overflow-hidden mt-2 ml-2 bg-base relative min-w-0 min-h-0">${t.raw(NodesView(map$, MapSize()))}</div>
     </div>`), scrollable$);
 }
