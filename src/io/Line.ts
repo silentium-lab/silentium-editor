ba8dada56464b8560689e034b6f44e40ccbb6600
Eugene Kosukhin <kosuha606@gmail.com>
2026-02-19 19:09:36 +0300

    feat: work on relations

--- src/io/Line.ts
+++ src/io/Line.ts
@@ -1,21 +1,34 @@
 import LinkerLine from 'linkerline';
-import { Computed, MessageType } from 'silentium';
+import { DestroyContainer, Message, MessageType } from 'silentium';
 import { TheNode } from '../domain/Node';
 
 export function Line(node$: MessageType<TheNode>) {
-  return Computed(node => {
-    const relations = node.arrows ?? [];
-    const fromEl = document.querySelector('.node-id-' + node.id);
-    if (fromEl && relations.length) {
-      relations.forEach(relation => {
-        const toEl = document.querySelector('.node-id-' + relation.id);
-        if (toEl) {
-          new LinkerLine({
-            start: fromEl,
-            end: toEl,
-          });
-        }
-      });
-    }
-  }, node$);
+  const dc = DestroyContainer();
+  return Message((resolve, reject) => {
+    node$.catch(reject).then((node) => {
+      dc.destroy();
+      const relations = node.arrows ?? [];
+      const fromEl = document.querySelector('.node-id-' + node.id);
+      if (fromEl && relations.length) {
+        relations.forEach(relation => {
+          const toEl = document.querySelector('.node-id-' + relation.id);
+          if (toEl) {
+            const line = new LinkerLine({
+              parent: document.body,
+              start: fromEl,
+              end: toEl,
+            });
+            dc.add(() => {
+              console.log('remove line');
+              line.remove();
+            });
+            resolve(line);
+          }
+        });
+      }
+    });
+    return () => {
+      dc.destroy();
+    };
+  });
 }
