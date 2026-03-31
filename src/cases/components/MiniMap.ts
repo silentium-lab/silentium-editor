0d7495ab42a5b20554aac5590a3e44b77a03aaaa
Eugene Kosukhin <kosuha606@gmail.com>
2026-03-31 15:27:29 +0300

    fix: minimap fix of removed dom nodes

--- src/cases/components/MiniMap.ts
+++ src/cases/components/MiniMap.ts
@@ -1,6 +1,6 @@
 import { All, Applied, Connected, Context, MessageType } from 'silentium';
 import { Task, Template } from 'silentium-components';
-import { ClassName, html, Id } from 'silentium-ui';
+import { ClassName, html, Id, Mount } from 'silentium-ui';
 import { ThePoint } from '../../domain/Point';
 import { ElementGeometry } from '../../io/ElementGeometry';
 import { Element } from 'silentium-web-api';
@@ -32,18 +32,22 @@ export function MiniMap(map$: MessageType<TheMap>) {
             class="minimap-preview ${t.raw(id$)} opacity-75 bg-primary border-primary border-solid"
           ></div>
           ${t.raw(
-            Applied(map$, map =>
-              Object.values(map.objects)
-                .map(
-                  object =>
-                    html`<div
-                      class="bg-red-500 absolute top-0 left-0 opacity-75"
-                      style="width: ${object.width * scale}px; height: ${object.height *
-                      scale}px;transform: translate(${object.position[0] * scale}px,${object
-                        .position[1] * scale}px)"
-                    ></div>`
-                )
-                .join('')
+            Mount(
+              Applied(
+                map$,
+                map =>
+                  `<div class="mini-elements">${Object.values(map.objects)
+                    .map(
+                      object =>
+                        html`<div
+                          class="bg-red-500 absolute top-0 left-0 opacity-75"
+                          style="width: ${object.width * scale}px; height: ${object.height *
+                          scale}px;transform: translate(${object.position[0] * scale}px,${object
+                            .position[1] * scale}px)"
+                        ></div>`
+                    )
+                    .join('')}</div>`
+              )
             )
           )}
         </div>`
