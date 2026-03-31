2e2d0cee095e9ae5dc23eb749c0eae6e565412d6
Eugene Kosukhin <kosuha606@gmail.com>
2026-03-31 14:56:43 +0300

    feat: types stream and translations

--- src/flows/MapModel.ts
+++ src/flows/MapModel.ts
@@ -8,45 +8,49 @@ import { MapTypeModel } from './MapTypeModel';
 import { SettingsModel } from './SettingsModel';
 
 export class MapModel {
-    public constructor(private map$: MessageSourceType<TheMap>) { }
+  public constructor(private map$: MessageSourceType<TheMap>) {}
 
-    public addType = (data: TheNodeType) => {
-        const newType = new MapTypeModel(this.map$, Date.now().toString());
-        newType.save(data);
-        return this;
-    }
+  public addType = (data: TheNodeType) => {
+    const newType = new MapTypeModel(this.map$, Date.now().toString());
+    newType.save(data);
+    return this;
+  };
 
-    public addObject = (type: TheNodeType, position: ThePosition) => {
-        const object = NodeNew(type, position);
-        const newObject = new MapObjectModel(this.map$, object.id);
-        newObject.save(object);
-        return this;
-    }
+  public addObject = (type: TheNodeType, position: ThePosition) => {
+    const object = NodeNew(type, position);
+    const newObject = new MapObjectModel(this.map$, object.id);
+    newObject.save(object);
+    return this;
+  };
 
-    public message() {
-        return this.map$;
-    }
+  public message() {
+    return this.map$;
+  }
 
-    public type(id: string) {
-        return new MapTypeModel(this.map$, id);
-    }
+  public types() {
+    return Applied(this.map$, map => Object.values(map.types));
+  }
 
-    public object(id: string) {
-        return new MapObjectModel(this.map$, id);
-    }
+  public type(id: string) {
+    return new MapTypeModel(this.map$, id);
+  }
 
-    public settings(): SettingsModel {
-        return new SettingsModel(this.map$);
-    }
+  public object(id: string) {
+    return new MapObjectModel(this.map$, id);
+  }
 
-    public activeObject() {
-        const activeId$ = Context<{ id: string }>('active-node-id');
-        const map = Value(this.map$);
-        return Applied(activeId$, (id) => {
-            if (map.value.objects[id.id]) {
-                return map.value.objects[id.id];
-            }
-            throw new Error(`MapModel: object with id #${id.id} was not found!`);
-        });
-    }
+  public settings(): SettingsModel {
+    return new SettingsModel(this.map$);
+  }
+
+  public activeObject() {
+    const activeId$ = Context<{ id: string }>('active-node-id');
+    const map = Value(this.map$);
+    return Applied(activeId$, id => {
+      if (map.value.objects[id.id]) {
+        return map.value.objects[id.id];
+      }
+      throw new Error(`MapModel: object with id #${id.id} was not found!`);
+    });
+  }
 }
