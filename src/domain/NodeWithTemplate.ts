e84966e165f8634e5fb5aef7ef7e7d3134eab0d9
Eugene Kosukhin <kosuha606@gmail.com>
2026-03-31 14:56:04 +0300

    fix: node with template fix type search

--- src/domain/NodeWithTemplate.ts
+++ src/domain/NodeWithTemplate.ts
@@ -1,12 +1,15 @@
 import { TheNode, TheNodeWithTemplate } from './Node';
 import { TheNodeType } from './NodeType';
 
+const notFoundTemplateMarkup =
+  '<div style="background:red;padding:10px;color:white">Template Not Found! id="$id"</div>';
+
 export function NodeWithTemplate(object: TheNode, types: TheNodeType[]): TheNodeWithTemplate {
-  const type = types.find(ct => ct.name === object.type);
+  const type = types.find(ct => ct.name === object.type || ct.id === object.type);
   if (!type) {
     return {
       node: object,
-      template: '',
+      template: notFoundTemplateMarkup.replace('$id', object.type),
     };
   }
   let { markup } = type;
