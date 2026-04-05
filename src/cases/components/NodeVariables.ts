ff85db949b75e86249c83d994905a28bf7fef2c0
Eugene Kosukhin <kosuha606@gmail.com>
2026-04-05 11:37:44 +0300

    fix: edit vars

--- src/cases/components/NodeVariables.ts
+++ src/cases/components/NodeVariables.ts
@@ -1,41 +1,36 @@
-import { Applied, Late, MessageSourceType, MessageType, Value } from "silentium";
-import { Template } from "silentium-components";
-import { html } from "silentium-ui";
+import { All, Applied, Connected, Late, Map, MessageSourceType, MessageType, Of, Value } from "silentium";
+import { Path, Template } from "silentium-components";
+import { html, Input } from "silentium-ui";
 import { Tr } from "../../io/Translation";
 
-export function NodeVariables(arrows$: MessageSourceType<Record<string, string>) {
-    const deleted$ = Late<string>();
-    const arrows = Value(arrows$);
-    deleted$.then(id => {
-        arrows$.use([...(arrows.value ?? []).filter(arrow => arrow.id !== id)]);
-    });
+export function NodeVariables(vars$: MessageSourceType<Record<string, string>>) {
+    const entries$ = Applied(vars$, (v) => Object.entries(v));
     return Template(
         t =>
             html`<div>
-        ${t.raw(
-                Applied(Map(arrows$, NodeVariable.bind(null, deleted$)), arr =>
+            ${t.raw(
+                Applied(Map(entries$, NodeVariable.bind(null, vars$)), arr =>
                     arr.length ? arr.join('') : '-'
                 )
             )}
-      </div>`
+        </div>`
     );
 }
 
-function NodeVariable(deleted$: MessageSourceType<string>, arrow: MessageType<[string, string]>) {
-    const currentArrow = Value(arrow);
-    const clicked = Late();
-    clicked.then(() => {
-        if (currentArrow.value) {
-            deleted$.use(currentArrow.value.id);
-        }
-    });
-    return Template(
+function NodeVariable(vars$: MessageSourceType<Record<string, string>>, theVar: MessageType<[string, string]>) {
+    const varModel = Late<string>('');
+    const sub = varModel.chain(Path(theVar, '1'));
+    const varVal = Value(theVar);
+    All(Value(Of(vars$)), varModel).then(((vars, v)) => {
+        vars$.use({
+            ...vars.value,
+        })
+    })
+    return Connected(Template(
         t =>
             html`<div class="mb-1">
-        ${t.escaped(Tr('Relation with'))} #${t.escaped(Path(arrow, 'id'))}
-        ${t.raw(
-                Button('&times;', 'btn h-5 w-5 p-0 inline-flex justify-center items-center', clicked)
-            )}
+        ${t.escaped(Tr('Relation with'))} #${t.escaped(Path(theVar, '0'))}
+        ${t.raw(Input(varModel))}
       </div>`
-    );
+    ), sub);
 }
